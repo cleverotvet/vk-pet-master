41008082134e1d7fd9ff2207ea4332c511b14af2
surkoff <ucozer@list.ru>
2019-08-19 12:11:33 +0600

    Updates

---
+++ precache-manifest.b013131455cbed9f96f1b8d3d2598dd3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "7050741184515dee356f0c48c63ec761",
+    "url": "./static/media/logo.70507411.jpg"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "e1c253e2f36311fd98de",
+    "url": "./static/js/main.ebaba853.chunk.js"
+  },
+  {
+    "revision": "8fed96d4fbcfffb88afe",
+    "url": "./static/js/2.4e790ae9.chunk.js"
+  },
+  {
+    "revision": "e1c253e2f36311fd98de",
+    "url": "./static/css/main.88e26b24.chunk.css"
+  },
+  {
+    "revision": "8fed96d4fbcfffb88afe",
+    "url": "./static/css/2.e4b48908.chunk.css"
+  },
+  {
+    "revision": "b2fb740bcec0cb4aa04badd37c6f8157",
+    "url": "./index.html"
+  }
+];
